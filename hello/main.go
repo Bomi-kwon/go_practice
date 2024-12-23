fe5fc238ecc484285c0beef84d3341e149d2f7ad
Bomi Kwon <bomi_kwon@postmath.co.kr>
2024-12-23 18:08:46 +0900

    화면에 이미지 출력

--- hello/main.go
+++ hello/main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv" // 타입 변환
 )
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	values := r.URL.Query()    // 쿼리 파라미터를 가져오는 함수
-	name := values.Get("name") // key가 name인 값을 가져오는 함수
-	if name == "" {
-		name = "World"
-	}
-	id, _ := strconv.Atoi(values.Get("id")) // key가 id인 값(string)을 가져와서 정수로 변환
-	fmt.Fprintf(w, "Hello, %s! id: %d", name, id)
-}
-
 func main() {
-	mux := http.NewServeMux() // 라우터 생성
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-	mux.HandleFunc("/bar", barHandler)
-
-	http.ListenAndServe(":8080", mux) // mux 인스턴스
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	// http://localhost:8080/파일이름.png 입력시 화면에 이미지 출력됨
+	http.ListenAndServe(":8080", nil) // mux 인스턴스
 }
